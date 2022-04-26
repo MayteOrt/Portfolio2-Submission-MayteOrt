12ff902e34b1662c7a7eba566bb9efb44262845e
MayteOrt <97573381+MayteOrt@users.noreply.github.com>
2022-04-25 21:31:48 -0400

    change

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    INCLUDE("database.php");
+?>
+<html>
+  <head>
+    <title>Bake Sale</title>
+  </head>
+  <body>
+   <h2>Sign Up for Baked Goods Donations for our 3rd Grade Bake Sale!</h2>
+    <?php
+       connect();
+       salesTable();
+       updateTable();
+       close();  
+    ?>
+    <form action= "index.php" method="POST">
+      
+      <input type="number" placeholder="Customer ID"  name="customer_id"/>
+      
+      <input type="text" placeholder="First Name"  name="first_name"/>
+      
+      <input type="text" placeholder="Last Name" name="last_name"/>
+      
+      <input type="text" placeholder="City" name="city"/>
+      
+      <input type="text" placeholder="State"  name="state"/>
+      
+      <button type= "submit">Submit</button>
+  </form>
+  </body>
+</html>
